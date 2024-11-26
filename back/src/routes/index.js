4f44fa5cf4a712491bfa1121fdf36465ff114873
George Samuel <126299897+GeorgeSamuel92@users.noreply.github.com>
2024-11-25 21:12:24 -0300

    atulização

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import {UsuarioController} from "../controllers/UsuarioController.js"
 import {TarefaController} from "../controllers/TarefaController.js"
@@ -8,13 +7,5 @@ router.post('/novoUsuario', UsuarioController.novoUsuario);
 router.get('/listarUsuarios', UsuarioController.listarUsuarios);
 router.post('/novaTarefa', TarefaController.novaTarefa);
 router.get('/listarTarefas', TarefaController.listarTarefas);
-=======
-import {Router} from 'express';
-import {UsuarioController} from '../controles/UsuarioController.js'
-
-const router = Router(); 
-
-router.post('/novoUsuario', UsuarioController.NovoUsuario);
->>>>>>> 1c013276b611b451ea251b768094a0cec1e69c2e
 
 export default router;
