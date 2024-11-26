c304c61e18ea392526a3cc7dfad54b73a3418ec2
George Samuel <126299897+GeorgeSamuel92@users.noreply.github.com>
2024-11-25 21:25:30 -0300

    atualização

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -1,19 +1,5 @@
 import mysql2 from 'mysql2/promise.js';
 
-<<<<<<< HEAD
-const connection = async () =>{
-    if (global.connection && global.connection.state !== 'disconnected'){
-        return global.connection;
-
-    }
-    const con = await mysql2.createConnection({
-        host: 'localhost',
-        port: '3306',
-        database: 'simulado_saep',
-        user:'root',
-        password: '1234',
-        multipleStatements: true
-=======
 const connection = async () => {
     if (global.connection && global.connection.state !== 'disconnect'){
         return global.connecton;
@@ -25,7 +11,6 @@ const connection = async () => {
         user: 'root',
         password: '1234',
         multipleStartements: true
->>>>>>> 1c013276b611b451ea251b768094a0cec1e69c2e
     })
 
     global.connection = con;
--- back/src/models/UsuarioModel.js
+++ back/src/models/UsuarioModel.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import connection from'../config/db.js';
 
 class Usuario{
     constructor(pUsuario){
-=======
-import connection from '../config/db.js';
-
-class Usuario{
-    constructor (pUsuario) {
->>>>>>> 1c013276b611b451ea251b768094a0cec1e69c2e
         this.nome = pUsuario.nome;
         this.email = pUsuario.email;
     }
 
     async insertUsuario(){
         try {
-<<<<<<< HEAD
             const conn= await connection();
             const pSql = "INSERT INTO USUARIO (NOME, EMAIL) VALUES (?,?)";
             const pValues = [this.nome, this.email];
@@ -37,19 +29,6 @@ class Usuario{
             return rows;
         } catch (error) {
             throw error;
-=======
-            const conn = await connection();
-            const pSql = "INSERT INTO USUARIO (NOME, EMAIL) VALUES (?,?);"
-            const pValues = [this.nome, this.email]
-            const [result] = await conn.query(pSql, pValues)
-            console.log(result);
-
-            return result;
-        }catch (error) {
-            throw error;
-        }finally {
-            await conn.release();
->>>>>>> 1c013276b611b451ea251b768094a0cec1e69c2e
         }
     }
 }
